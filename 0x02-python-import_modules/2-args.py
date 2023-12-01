e2702115e9b3fb62e521a3033a17c888dec98632
osama <osamaelsafty67@gmail.com>
2023-11-30 16:55:44 -0800

    vefvddv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-# Check if the script is being run as the main program
 if __name__ == "__main__":
     import sys
-    # Get the number of arguments
-    num_args = len(sys.argv) - 1  # subtract 1 to exclude the script name
 
-    # Print the number of arguments and whether it's singular or plural
-    print("{} argument{}".format(num_args, "s" if num_args != 1 else ""), end="")
-
-    # Check if there are any arguments
-    if num_args > 0:
-        print(":")  # Print a colon before listing the arguments
-
-        # Iterate through the arguments and print their position and value
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print(".")
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
